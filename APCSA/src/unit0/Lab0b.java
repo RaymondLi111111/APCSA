a9141e577b41f3e0b586f962206fd0664c14a074
lir1303 <lir1303@CA-SU-F106-19.sduhsd.lan>
2018-02-02 13:57:13 -0800

    Upload APCSA project!

---
+++ APCSA/src/unit0/Lab0b.java
@@ -0,0 +1,75 @@
+package unit0;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab0b
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = 32000;
+		int intOne = 2147209438;
+		long longOne = 1000000000000l;
+
+		//decimal variables
+		float floatOne = 12345614124124124124124124.00f;
+		double doubleOne = 1234567087654235467865567.45678546786757467868;
+
+		//other integer types
+		char charOne = 'a';
+		char charTwo = 98;
+
+		//other types
+		boolean boolOne = true;
+		String stringOne = "gucci gang gucci gang";
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Jim Bob                08/18/06*");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*          Real Types           *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*      Other Integer Types      *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*          Other Types          *");
+		System.out.println("*                               *");
+		System.out.println("*boolOne = "+boolOne+"\t\t*");
+		System.out.println("*StringOne = "+stringOne+"\t\t*");
+		
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
