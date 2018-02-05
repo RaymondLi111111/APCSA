428a30d2a211f1d337cccf87ddff5562ddf0c1ad
RaymondLi111111 <36137578+RaymondLi111111@users.noreply.github.com>
2018-02-05 14:48:49 -0800

    Add files via upload

---
+++ unit1/SmileyFace.java
@@ -0,0 +1,45 @@
+package unit1;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Canvas;
+
+public class SmileyFace extends Canvas
+{
+ public SmileyFace()    //constructor - sets up the class
+ {
+    setSize(800,600);
+    setBackground(Color.WHITE);
+    setVisible(true);
+ }
+
+ public void paint( Graphics window )
+ {
+    smileyFace(window);
+ }
+
+ public void smileyFace( Graphics window )
+ {
+    window.setColor(Color.BLUE);
+    window.drawString("SMILEY FACE LAB ", 35, 35);
+
+    window.setColor(Color.YELLOW);
+    window.fillOval( 210, 100, 400, 400 );
+
+		//add more code here
+    window.setColor(Color.BLACK);
+    window.fillOval( 300, 200, 40, 40 );
+
+    window.fillOval( 450, 200, 40, 40 );
+    
+    window.drawArc(300, 300, 420, 100, 0, 180);
+
+
+ }
+}
---
+++ unit1/StarsAndStripes.java
@@ -0,0 +1,40 @@
+package unit1;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+ public StarsAndStripes()
+ {
+    out.println("StarsAndStripes");
+    printTwoBlankLines();
+ }
+
+ public void printTwentyStars()
+ {
+	 out.println("******************");
+ }
+
+ public void printTwentyDashes()
+ {
+	 out.println("--------------------");
+ }
+
+ public void printTwoBlankLines()
+ {
+ }
+ 
+ public void printASmallBox()
+ {	
+ }
+
+ public void printABigBox()
+ { 	
+ }   
+}
