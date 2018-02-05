9cac7287e126796f00184425165d1d73692dbad9
RaymondLi111111 <36137578+RaymondLi111111@users.noreply.github.com>
2018-02-05 14:58:30 -0800

    Add files via upload

--- unit1/RectangleMaker.java
+++ unit1/RectangleMaker.java
@@ -1,5 +1,18 @@
 package unit1;
 
-public class RectangleMaker {
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Canvas;
 
+class RectangleMaker extends Canvas {
+	 public RectangleMaker()    //constructor method - sets up the class
+	 {
+	    setSize(800,600);
+	    setBackground(Color.WHITE);   	
+	    setVisible(true);
+	 }
+	 public void paint( Graphics window ) {
+		 window.fillRect(420, 420, 6, 7);
+		 window.fillRect(333, 333, 20, 1);
+	 }
 }
